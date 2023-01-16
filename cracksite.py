3ca26c2465ee0a24c5ef19ad870cc850a20e763b
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-01-16 21:32:08 +0300

    Поиск школьника вынесен в отдельную функцию

--- cracksite.py
+++ cracksite.py
@@ -2,10 +2,19 @@ from django.core.exceptions import MultipleObjectsReturned, ObjectDoesNotExist
 
 from datacenter.models import Mark, Chastisement, Commendation, Schoolkid, Subject, Lesson
 
+def get_schoolkid_by_name(name):
+    try:
+        schoolkid = Schoolkid.objects.get(full_name__contains=name)
+        return schoolkid
+    except ObjectDoesNotExist:
+        print(f"Не найдено ученика по имени {name}")
+    except MultipleObjectsReturned:
+        print(f"Найдено более одного ученика по имени {name}")
+
 
 def fix_marks(schoolkid_name):
     try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        schoolkid = get_schoolkid_by_name(schoolkid_name)
         marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
         for mark in marks:
             mark.points = 5
@@ -14,20 +23,17 @@ def fix_marks(schoolkid_name):
         print(f"Не найдено ученика по имени {schoolkid_name}")
     except MultipleObjectsReturned:
         print(f"Найдено более одного ученика по имени {schoolkid_name}")
-
 def remove_chastisements(schoolkid_name):
     try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        schoolkid = get_schoolkid_by_name(schoolkid_name)
         Chastisement.objects.filter(schoolkid=schoolkid).delete()
     except ObjectDoesNotExist:
         print(f"Не найдено ученика по имени {schoolkid_name}")
     except MultipleObjectsReturned:
         print(f"Найдено более одного ученика по имени {schoolkid_name}")
-
-
 def create_commendation(schoolkid_name, subject_name):
     try:
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
+        schoolkid = get_schoolkid_by_name(schoolkid_name)
         subject = Subject.objects.filter(title=subject_name, year_of_study=schoolkid.year_of_study)[0]
         lesson = Lesson.objects.filter(group_letter=schoolkid.group_letter, year_of_study=schoolkid.year_of_study, subject=subject).order_by('date').first()
         #for lesson in lessons:
